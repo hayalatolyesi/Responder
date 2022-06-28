45d304c3e2a0621b1cc88fab887e047fa42440b8
erdal_digidea <erdal@digidea.com.tr>
2022-06-28 18:47:42 +0300

    Change Response type

---
+++ src/Info copy.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Erdal\Responder;
+
+/**
+*@Author-Name   : Erdal EROĞLU 
+*@Author-Mail   : erdal.eroglu@gmail.com
+*@Create-Date   : dd-mm-YYYY
+**/
+class Info implements IResponder{
+    
+    /*
+    * 
+    * For Response Messages
+    *
+    * $message string
+    * 
+    */    
+    public $message;
+    
+    /*
+    * 
+    * This Object, Identity Name
+    *
+    * CONST STATUS string
+    * 
+    */    
+    CONST STATUS ="info";
+    
+    /*
+    *
+    *  @param string $message 
+    *
+    *  @return void
+    *
+    */
+
+    public function set(String $message):void{
+        
+        $this->message  =$message;
+        
+    }
+    
+}
