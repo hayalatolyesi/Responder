7f3c7985aeab8bcd85cf209bd4221ca2b26f55c8
erdal-eroglu-sirius <erdal.eroglu@siriushs.com>
2025-02-24 14:28:19 +0300

    Response tipi olarak result eklendi (Sonuç true yada false)

---
+++ src/Result.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Erdal\Responder;
+
+/**
+*@Author-Name   : Erdal EROĞLU 
+*@Author-Mail   : erdal.eroglu@gmail.com
+*@Create-Date   : dd-mm-YYYY
+**/
+class Result implements IResponder{
+    
+    /** 
+    * 
+    * For Response Messages
+    *
+    * $message string
+    * 
+    */    
+    public bool $message;
+    
+    /** 
+    * 
+    * This Object, Identity Name
+    *
+    * CONST STATUS string
+    * 
+    */    
+    CONST STATUS ="result";
+    
+    /** 
+    *
+    *  @param string $message (Not Null)
+    *
+    *  @return void
+    *
+    */
+
+    public function set(boolean $message):void{
+        
+        $this->message  = $message;
+
+    }
+    
+}
